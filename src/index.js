e67ca4bb972db645ec71d87914aefea5235e2d06
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-26 17:42:12 +0200

    Step 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import { initMongoCollection } from './db/initMongoCollection.js';
 import { startServer } from './server.js';
+
+const bootstrap = async () => {
+  await initMongoCollection();
+  startServer();
+};
+
+bootstrap();
 
 startServer();
