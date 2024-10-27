f8793376b2e4ab3942fd5e5d40f336870942e091
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-27 13:36:07 +0100

    another log

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ export const startServer = () => {
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
+    console.log(contacts);
     res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
