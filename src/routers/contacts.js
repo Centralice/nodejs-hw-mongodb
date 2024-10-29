81d4393aa514a27b68917af26cf92b1e098389eb
Centralice <108938330+Centralice@users.noreply.github.com>
2024-10-29 18:40:39 +0100

    lecture 1

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import * as contactControllers from '../controllers/contacts.js';
-
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 const contactsRouter = Router();
 
-contactsRouter.get('/', contactControllers.getContactsController);
+contactsRouter.get('/', ctrlWrapper(contactControllers.getContactsController));
 
-contactsRouter.get('/:contactId', contactControllers.getContactByIdController);
+contactsRouter.get(
+  '/:contactId',
+  ctrlWrapper(contactControllers.getContactByIdController),
+);
 
 export default contactsRouter;
