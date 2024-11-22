8ca4f2c32792eac53c993ba29f363d356c70e57d
Centralice <108938330+Centralice@users.noreply.github.com>
2024-11-22 15:06:48 +0100

    register

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,13 @@
 import UserCollection from '../db/models/User.js';
 import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 
 export const register = async (payload) => {
-  const { email } = payload;
+  const { email, password } = payload;
   const user = await UserCollection.findOne({ email });
   if (user) {
     throw createHttpError(409, 'Email in use');
   }
-  return UserCollection.create(payload);
+  const hashPassword = await bcrypt.hash(password, 10);
+  return UserCollection.create({ ...payload, password: hashPassword });
 };
